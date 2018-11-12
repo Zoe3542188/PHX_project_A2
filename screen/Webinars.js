3ffc1feb7bf9ada2fb38d8da27c01da6c90af47c
Zoe3542188 <lyl5380@bu.edu>
2018-11-12 01:19:31 -0500

    add Webinars

--- screen/Webinars.js
+++ screen/Webinars.js
@@ -2,29 +2,122 @@ import React, { Component } from "react";
 import {View,
 		Text,
 		StyleSheet,
-		Platform,
 		Button,
-		WebView
+		FlatList,
+		ScrollView,
+		AsyncStorage,
+		Image,
+		TouchableOpacity
 		} from "react-native";
 import styles from "./style";
-import VideoComponent from './VideoComponent';
+import { Navigation } from "react-native-navigation";
 
-class Webinars extends Component{
-  render() {
-    return (
-        <View>
-        <VideoComponent />
-        </View>
-    );
-  }
+console.disableYellowBox = true;
+
+class FlatListItem extends Component {
+    render() {          
+        return (
+        	<TouchableOpacity style={{
+                flex: 1,
+                flexDirection:'column',
+                backgroundColor: this.props.index % 2 == 0 ? 'white': 'white'}}>
+	          	<View style={{
+	          	  	flex:1,
+	          	  	flexDirection:'row'}}> 
+	          		<Image
+	          	  		source={{uri:this.props.item.img_url}}
+	          	  		style={{width:100, height:100, margin:5}}
+	          		/>
+		         	<View style={{
+		         		flex:1,
+		         		flexDirection:'column',
+		         		height:100
+		         	}}>
+		              <Text style={styles.flatListTitle}>{this.props.item.short_title}</Text>
+		              <Text style={styles.flatListItem}>{this.props.item.title}</Text>
+		            </View>
+	            </View>
+                <View style={{
+                    height:1,
+                    backgroundColor:'#5f6b71',
+                }}></View>
+        	</TouchableOpacity>
+        );
+    }
 }
 
-export default Webinars;
 
 
-      
+class WelcomeScreen extends Component{
+	state={
+        data:[]
+    };
+
+        fetchData = async () =>{
+        //response
+        const response = await fetch('https://populationhealthexchange.org/wp-json/wp/v2/posts?per_page=60');
+        //posts
+        const posts = await response.json();
+        var x;
+        var all_podcast = [];
+        //var Podcast = {};
+        for (x in posts){
+            var flag = JSON.stringify(posts[x].content.protected);
+            var type = JSON.stringify(posts[x].acf.feature_number);
+            if(flag == "false" && type == "988"){
+                var temp = JSON.stringify(posts[x].content.rendered);
+                var podcast_url = temp.match(/src=\\\"(\S*)\?/)[1];
+                var name = JSON.stringify(posts[x].acf.post_header);
+                var date = JSON.stringify(posts[x].date);
+                var author = JSON.stringify(posts[x].author);
+                var img_url_raw = JSON.stringify(posts[x].acf.feature_image.url);
+                var description = JSON.stringify(posts[x].acf.blurb);
+                var short_title = JSON.stringify(posts[x].title.rendered);
+                var img_url = img_url_raw.match(/\"(\S*)\"/)[1];
+                name = name.replace(/^\"|\"$/g,'');
+                name = name.replace(/[\\]/g,'');
+                short_title = short_title.replace(/^\"|\"$/g,'');
+                short_title = short_title.replace('FA ','');
+                description = description.replace(/^\"|\"$/g,'');
+                all_podcast.push({
+                    "title": name,
+                    "date": date,
+                    "author": author,
+                    "img_url": img_url,
+                    "description": description,
+                    "podcast_url": podcast_url,
+                    "short_title": short_title,
+                });
+            }
+        }
+        this.setState({data:all_podcast});
+        //alert(JSON.stringify(all_podcast[].description))
+    };
+				//<Button title="Click me" onPress={()=>this.goToScreen('Clickme')} />
+				//<Button title="Don't click me" onPress={()=>alert("I said don't!")}/>
+    componentDidMount(){
+        //fetch data right after page load
+        this.fetchData();
+    };
 
 
-      <View style={styles.container}>
-        <Text>View Courses here</Text>
-      </View>
+
+	render(){
+		return(
+			<View style={{flex: 1, marginTop: 22}}>
+            <FlatList 
+                data={this.state.data}
+                renderItem={({item, index})=>{
+                    return (
+                    <FlatListItem item={item} index={index}>
+                    </FlatListItem>);
+                }}
+                >
+
+            </FlatList>
+        	</View>
+		);
+	}
+}
+
+export default WelcomeScreen;
