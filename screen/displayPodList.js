569c84ba868880ca8241ac11433f1ad1da1d7a7b
Zoe3542188 <lyl5380@bu.edu>
2018-11-12 00:40:18 -0500

    flexbox

--- screen/displayPodList.js
+++ screen/displayPodList.js
@@ -6,84 +6,15 @@ import {View,
 		FlatList,
 		ScrollView,
 		AsyncStorage,
-    TouchableOpacity,
 		Image,
+		TouchableOpacity
 		} from "react-native";
-import {
-    Player,
-    Recorder,
-    MediaStates
-} from 'react-native-audio-toolkit';    
 import styles from "./style";
 import { Navigation } from "react-native-navigation";
-import { startTabs, startStacks} from './startMainTab';
 
-
-// export const audioStack = () => Navigation.setRoot({
-//   root: {
-//     stack: {
-//       id: 'App',
-//       children: [
-//         {
-//           component: {
-//             name: 'StackNav',
-//           }
-//         }
-//     ],
-//     }
-//   }
-// })
+console.disableYellowBox = true;
 
 class FlatListItem extends Component {
-  static get options() {
-      return {
-        topBar: {
-          title: {
-            text: 'Stream podcast'
-          },
-        }
-      };
-    }
-  goToScreen = (screenName) =>{
-    Navigation.push(this.props.componentId,{
-      component:{
-        name: screenName
-      }
-    })
-  }
-  backFromScreen = () =>{
-    Navigation.pop(this.props.componentId)
-  } 
-
-
-  stream() {
-    new Player(this.props.item.podcast_url).play();
-  }
-
-  render() {          
-    return (
-      <TouchableOpacity onPress={()=>this.stream()} style={{
-        flex: 1,
-        backgroundColor: this.props.index % 2 == 0 ? 'white': '#88daf7'                
-        }}>
-        <Image
-    		source={{uri:this.props.item.img_url}}
-    		style={{width:100, height:100, margin:5}}
-        /> 
-    	  <View style={{
-    	  	flex:1,
-    	  	flexDirection:'column'
-    	  }}>          
-          <Text style={styles.flatListItem}>{this.props.item.title}</Text>
-          <Text style={styles.flatListItem}>{this.props.item.description}</Text>
-        </View>
-      </TouchableOpacity>
-    );
-  }
-}
-
-
-class WelcomeScreen extends Component{
 	goToScreen = (screenName) =>{
 		Navigation.push(this.props.componentId,{
 			component:{
@@ -91,14 +22,43 @@ class WelcomeScreen extends Component{
 			}
 		})
 	};
+    render() {          
+        return (
+        	<TouchableOpacity style={{
+                flex: 1,
+                flexDirection:'column',
+                backgroundColor: this.props.index % 2 == 0 ? 'white': '#88daf7'}}>
+	          	<View style={{
+	          	  	flex:1,
+	          	  	flexDirection:'row'}}> 
+	          		<Image
+	          	  		source={{uri:this.props.item.img_url}}
+	          	  		style={{width:100, height:100, margin:5}}
+	          		/>
+		         	<View style={{
+		         		flex:1,
+		         		flexDirection:'column',
+		         		height:100
+		         	}}>
+		              <Text style={styles.flatListTitle}>{this.props.item.short_title}</Text>
+		              <Text style={styles.flatListItem}>{this.props.item.title}</Text>
+		            </View>
+	            </View>
+        	</TouchableOpacity>
+        );
+    }
+}
 
+
+
+class WelcomeScreen extends Component{
 	state={
         data:[]
     };
 
         fetchData = async () =>{
         //response
-        const response = await fetch('https://populationhealthexchange.org/wp-json/wp/v2/posts?per_page=30');
+        const response = await fetch('https://populationhealthexchange.org/wp-json/wp/v2/posts?per_page=60');
         //posts
         const posts = await response.json();
         var x;
@@ -115,7 +75,13 @@ class WelcomeScreen extends Component{
                 var author = JSON.stringify(posts[x].author);
                 var img_url_raw = JSON.stringify(posts[x].acf.feature_image.url);
                 var description = JSON.stringify(posts[x].acf.blurb);
+                var short_title = JSON.stringify(posts[x].title.rendered);
                 var img_url = img_url_raw.match(/\"(\S*)\"/)[1];
+                name = name.replace(/^\"|\"$/g,'');
+                name = name.replace(/[\\]/g,'');
+                short_title = short_title.replace(/^\"|\"$/g,'');
+                short_title = short_title.replace('FA ','');
+                description = description.replace(/^\"|\"$/g,'');
                 all_podcast.push({
                     "title": name,
                     "date": date,
@@ -123,6 +89,7 @@ class WelcomeScreen extends Component{
                     "img_url": img_url,
                     "description": description,
                     "podcast_url": podcast_url,
+                    "short_title": short_title,
                 });
             }
         }
--- screen/style.js
+++ screen/style.js
@@ -1,71 +1,41 @@
-// add all styles here
-const React = require("react-native");
+import {Button, View} from "react-native";
 
+const React = require("react-native");
 const { StyleSheet } = React;
 
-export default {
-	backgroundVideo: {
-	  position: 'absolute',
-	  top: 0,
-	  left: 0,
-	  bottom: 0,
-	  right: 0,
-	},	
+export default{
 	container:{
 		flex:1,
 		alignItems:'center',
 		justifyContent:'center'
 	},
-	containerView: {
-	  flex: 1,
-	},
-	commentTextInput: {
-	  height: 240,
-	  fontSize: 14,
-	  borderRadius: 5,
-	  borderWidth: 1,
-	  borderColor: '#eaeaea',
-	  backgroundColor: '#fafafa',
-	  paddingLeft: 10,
-	  marginLeft: 15,
-	  marginRight: 15,
-	  marginTop: 5,
-	  marginBottom: 5,
-	},	
+
+    flexRow:{
+        flexDirection:'row',
+        height:150,
+    },
+
 	flexColumn:{
 		flexDirection:'column',
 		alignItems:'stretch',
 	},
-	loginScreenContainer: {
-	  flex: 1,
+	itemImageDisplay:{
+		flex:1,
+        height: 150,
+		flexDirection:'row',
+
 	},
-	logoText: {
-	  fontSize: 20,
-	  fontWeight: "800",
-	  marginTop: 130,
-	  marginBottom: 30,
-	  textAlign: 'center',
+
+	flatListItem: {
+        color: 'black',
+        padding: 5,
+        fontSize: 16,  
 	},
-	loginFormView: {
-	  flex: 1
-	},
-	loginFormTextInput: {
-	  height: 43,
-	  fontSize: 14,
-	  borderRadius: 5,
-	  borderWidth: 1,
-	  borderColor: '#eaeaea',
-	  backgroundColor: '#fafafa',
-	  paddingLeft: 10,
-	  marginLeft: 15,
-	  marginRight: 15,
-	  marginTop: 5,
-	  marginBottom: 5,
-	},	
-	SubmitButton: {
-	  backgroundColor: '#3897f1',
-	  borderRadius: 5,
-	  height: 45,
-	  marginTop: 10,
-	},
-};
+
+	flatListTitle: {
+		color: 'black',
+		padding:5,
+		fontSize:22,
+		fontWeight:'bold',
+	}
+}
